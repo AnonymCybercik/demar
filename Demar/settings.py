830cbda9f3c8f8f7accdcdf08b214d718d74296a
anonymcybercik <xoja9336706@gmail.com>
2022-03-06 13:37:55 +0500

    asd

--- Demar/settings.py
+++ Demar/settings.py
@@ -125,7 +125,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles'),)
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'assets')]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
